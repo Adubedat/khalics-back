9a9270ce1107ec59ab2ef73f87142ee530079013
Renan PORCON <rporcon@student.42.fr>
2018-11-07 11:38:32 +0100

    init repo + lambda test

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'always'
+};
