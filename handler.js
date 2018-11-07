17e75dd3c509b9ce9b2c97a23c35425cc4077317
Renan PORCON <rporcon@student.42.fr>
2018-11-07 11:53:36 +0100

    eslint + prettier ok

--- handler.js
+++ handler.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
 module.exports.hello = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Go Serverless v1.0! Your function executed successfully!"
-            // input: event,
-        })
-    };
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'Go Serverless v1.0! Your function executed successfully!'
+			// input: event,
+		})
+	};
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+	// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+	// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.imageResize = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "resized your image !"
-        })
-    };
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'resized your image !'
+		})
+	};
+	// Use this code if you don't use the http event with the LAMBDA-PROXY integration
+	// return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
