8aeaff42350dec20f6f9a9c16f48a8d539c02d17
Renan PORCON <rporcon@student.42.fr>
2018-11-12 13:34:39 +0100

    userCreate basic ok

--- handler.js
+++ handler.js
@@ -10,26 +10,14 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// test async way
-module.exports.userCreate = function (event, context, callback) { // eslint-disable-line
+module.exports.userCreate = (event, context, callback) => { // eslint-disable-line
   const body = JSON.parse(event.body);
   const { pseudo, password, email } = { ...body };
-  // const test = { pseudo: 'abc', password: 'bdc', email: 'cdc' };
-  // const { pseudo, password, email } = { ...test };
-  // return { statusCode: 200 };
-  /* verify each field if well formated */
-  // console.log(body);
   const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
-  // const createUserQuery = 'INSERT INTO users (pseudo, password, email) VALUES ("ddd42o", "dddo42rd", "d42dd")';
   connection.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
     connection.end();
-    console.log('query');
     if (error) {
-      console.log('error');
-      callback(error, {
-        statusCode: 400,
-        // body: JSON.stringify({ error }),
-      });
+      callback(error, { statusCode: 400 });
     }
     callback(null, { statusCode: 200 });
   });
