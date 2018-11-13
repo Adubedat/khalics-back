420aa9338dbabeac58e9d3f3544ea7d6a7f6ed82
Renan PORCON <rporcon@student.42.fr>
2018-11-13 15:32:42 +0100

    test

--- services/user/create.js
+++ services/user/create.js
@@ -2,12 +2,12 @@ const db = require('../../lib/dbInit');
 
 module.exports.create = (event, context, callback) => { // eslint-disable-line
   // TODO: verify JSON.parse return
-  const body = JSON.parse(event.body);
+  // const body = JSON.parse(event.body);
   // TODO: verify fields
-  const { pseudo, password, email } = { ...body };
+  // const { pseudo, password, email } = { ...body };
   // TODO: sql injection verif
-  const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
-  callback(null, { statusCode: 200 });
+  // const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
+  const createUserQuery = 'INSERT INTO users (pseudo, password, email) VALUES ("qqq", "dddd", "sss")';
   db.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
     db.end();
     if (error) {
