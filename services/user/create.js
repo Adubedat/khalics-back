04c141fa8571abf7c3009b2d3f69373c150cef18
Arthur DUBEDAT <adubedat@e1r9p2.42.fr>
2018-11-13 15:54:43 +0100

    local test with event.json works

--- services/user/create.js
+++ services/user/create.js
@@ -2,12 +2,13 @@ const db = require('../../lib/dbInit');
 
 module.exports.create = (event, context, callback) => { // eslint-disable-line
   // TODO: verify JSON.parse return
-  // const body = JSON.parse(event.body);
+  console.log(event);
+  const body = JSON.parse(event.body);
   // TODO: verify fields
-  // const { pseudo, password, email } = { ...body };
+  const { pseudo, password, email } = { ...body };
   // TODO: sql injection verif
-  // const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
-  const createUserQuery = 'INSERT INTO users (pseudo, password, email) VALUES ("qqq", "dddd", "sss")';
+  const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
+  // const createUserQuery = 'INSERT INTO users (pseudo, password, email) VALUES ("qqq", "dddd", "sss")';
   db.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
     db.end();
     if (error) {
