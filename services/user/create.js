2c29365b7b8462c11e4fc1f0cda3fa40b127095d
Renan PORCON <rporcon@student.42.fr>
2018-11-13 14:07:55 +0100

    structure

---
+++ services/user/create.js
@@ -0,0 +1,17 @@
+import db from '../../lib/dbInit';
+
+module.exports.create = (event, context, callback) => { // eslint-disable-line
+  // TODO: verify JSON.parse return
+  const body = JSON.parse(event.body);
+  // TODO: verify fields
+  const { pseudo, password, email } = { ...body };
+  // TODO: sql injection verif
+  const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
+  db.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
+    db.end();
+    if (error) {
+      callback(error, { statusCode: 400 });
+    }
+    callback(null, { statusCode: 200 });
+  });
+};
