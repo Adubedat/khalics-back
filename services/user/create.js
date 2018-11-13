80d98a8b2e9df016844b4853124a73f21c3343a6
Renan PORCON <rporcon@student.42.fr>
2018-11-13 14:46:32 +0100

    dbInit bug

--- services/user/create.js
+++ services/user/create.js
@@ -1,17 +1,23 @@
 import db from '../../lib/dbInit';
 
 module.exports.create = (event, context, callback) => { // eslint-disable-line
+  console.log('--->');
   // TODO: verify JSON.parse return
-  const body = JSON.parse(event.body);
+  // const body = JSON.parse(event.body);
   // TODO: verify fields
-  const { pseudo, password, email } = { ...body };
+  // const { pseudo, password, email } = { ...body };
   // TODO: sql injection verif
-  const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
-  db.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
-    db.end();
-    if (error) {
-      callback(error, { statusCode: 400 });
-    }
-    callback(null, { statusCode: 200 });
-  });
+  // const createUserQuery = `INSERT INTO users (pseudo, password, email) VALUES ("${pseudo}", "${password}", "${email}")`;
+  const createUserQuery = 'INSERT INTO users (pseudo, password, email) VALUES ("pseudo", "pwd", "ok@ok.fr")';
+  console.log('AAAOO');
+  callback(null, { statusCode: 200 });
+  // db.query(createUserQuery, (error, results, fields) => { // eslint-disable-line
+  //   console.log('AAA');
+  //   db.end();
+  //   if (error) {
+  //     callback(error, { statusCode: 400 });
+  //   }
+  //   console.log('LOOO');
+  //   callback(null, { statusCode: 200 });
+  // });
 };
