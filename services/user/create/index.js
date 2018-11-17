f70c837ea0f8ce150993fe503231dc88e9e5d163
akio <rporcon@student.42.fr>
2018-11-17 23:08:23 +0100

    email exist raise error (pre sign up lambda trigger)

--- services/user/create/index.js
+++ services/user/create/index.js
@@ -5,9 +5,6 @@ const cognitoIdp = new AWS.CognitoIdentityServiceProvider();
 // const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => { // eslint-disable-line
-  // eslint-disable-next-line
-  // context.callbackWaitsForEmptyEventLoop = false;
-  // console.log('event:', event);
   if (event.request.userAttributes.hasOwnProperty('email')) { // eslint-disable-line
     const { email } = event.request.userAttributes;
     const params = {
@@ -18,12 +15,9 @@ module.exports.handler = (event, context, callback) => { // eslint-disable-line
     cognitoIdp.listUsers(params).promise()
       .then((results) => {
         const { userName } = event;
-        // console.log('results', JSON.stringify(results));
-        // if the usernames are the same, dont raise and error here so that
-        // cognito will raise the duplicate username error
+        // if usernames are the same dont raise error to let cognito handle it
         if (results.Users.length > 0 && results.Users[0].Username !== userName) {
-          // console.log(`Duplicate email address in signup. ${email}`);
-          callback(new Error('A user with the same email address exists'));
+          callback(new Error('emailExists'));
         }
         callback(null, event);
       })
@@ -34,7 +28,7 @@ module.exports.handler = (event, context, callback) => { // eslint-disable-line
   }
 
   // Below in another lambda func
-  // On PostConfirmation_ConfirmSignUp trigger
+  // On PostConfirmation_ConfirmSignUp trigger (may not be necessary)
   // Useless to have user in database if not confirmed (it's already in cognito)
   //
   // const timestamp = new Date().getTime();
