8cadcc0065e6cc0a4af30375bbb4851eb0fce8e3
Renan PORCON <rporcon@student.42.fr>
2018-11-23 11:49:00 +0100

    find same email with dynamodb work in local

--- services/user/preSignupCognitoTrigger/index.js
+++ services/user/preSignupCognitoTrigger/index.js
@@ -1,26 +1,49 @@
 const AWS = require('aws-sdk');
 
-const cognitoIdp = new AWS.CognitoIdentityServiceProvider();
+// const cognitoIdp = new AWS.CognitoIdentityServiceProvider();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => { // eslint-disable-line
   if (event.request.userAttributes.hasOwnProperty('email')) { // eslint-disable-line
-    const { email } = event.request.userAttributes;
-    const params = {
-      UserPoolId: event.userPoolId,
-      Filter: `email = "${email}"`,
-    };
+  const { email, username } = event.request.userAttributes;
 
-    cognitoIdp.listUsers(params).promise()
-      .then((results) => {
-        const { userName } = event;
-        // if usernames are the same dont raise error to let cognito handle it
-        if (results.Users.length > 0 && results.Users[0].Username !== userName) {
-          callback(new Error('emailExists'));
-        }
-        callback(null, event);
-      })
-      .catch((error) => {
-        callback(error);
-      });
-  }
+  const username = 'Google_100624641438488386206';
+  const dbParams = {
+    TableName: 'users',
+    KeyConditionExpression: 'username = :username',
+    ExpressionAttributeValues: {
+      ':username': `${username} + a`,
+    },
+    ProjectionExpression: 'email',
+    Limit: 1,
+  };
+
+  dynamoDb.query(dbParams, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      if (data.count === 0) {
+        callback(new Error('emailExists'));
+      }
+      callback(null, event);
+    }
+  });
+
+  // const idpParam = {
+  //   UserPoolId: event.userPoolId,
+  //   Filter: `email = "${email}"`,
+  // };
+  // cognitoIdp.listUsers(idpParam).promise()
+  //   .then((results) => {
+  //     const { userName } = event;
+  //     // if usernames are the same dont raise error to let cognito handle it
+  //     if (results.Users.length > 0 && results.Users[0].Username !== userName) {
+  //       callback(new Error('emailExists'));
+  //     }
+  //     callback(null, event);
+  //   })
+  //   .catch((error) => {
+  //     callback(error);
+  //   });
+  // }
 };
