5568dfd0aba618e35cb9de2941011f6607a77f20
Renan PORCON <rporcon@student.42.fr>
2018-11-23 14:04:43 +0100

    email verif db ok

--- services/user/preSignupCognitoTrigger/index.js
+++ services/user/preSignupCognitoTrigger/index.js
@@ -4,30 +4,34 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => { // eslint-disable-line
-  if (event.request.userAttributes.hasOwnProperty('email')) { // eslint-disable-line
-  const { email, username } = event.request.userAttributes;
-
-  const username = 'Google_100624641438488386206';
+  if (!event.request.userAttributes.hasOwnProperty('email')) { // eslint-disable-line
+    callback(new Error('email userAttributes do not exist'));
+  }
+  const { email } = event.request.userAttributes;
   const dbParams = {
     TableName: 'users',
-    KeyConditionExpression: 'username = :username',
+    FilterExpression: 'email = :email',
     ExpressionAttributeValues: {
-      ':username': `${username} + a`,
+      ':email': email,
     },
     ProjectionExpression: 'email',
-    Limit: 1,
   };
 
-  dynamoDb.query(dbParams, (err, data) => {
+  const onScan = (err, data) => {
     if (err) {
       callback(err);
-    } else {
-      if (data.count === 0) {
-        callback(new Error('emailExists'));
-      }
-      callback(null, event);
     }
-  });
+    if (data.Count === 1) {
+      callback(new Error('emailExists'));
+    }
+    if (typeof data.LastEvaluatedKey !== 'undefined') {
+      dbParams.ExclusiveStartKey = data.LastEvaluatedKey;
+      dynamoDb.scan(dbParams, onScan);
+    }
+    callback(null, event);
+  };
+    // WARNING: scan can retrieve a maximum of 1MB of data
+  dynamoDb.scan(dbParams, onScan);
 
   // const idpParam = {
   //   UserPoolId: event.userPoolId,
@@ -45,5 +49,4 @@ module.exports.handler = (event, context, callback) => { // eslint-disable-line
   //   .catch((error) => {
   //     callback(error);
   //   });
-  // }
 };
