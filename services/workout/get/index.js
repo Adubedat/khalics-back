6c354a3fd9411db8fa73620ef03a1a59991f170a
Renan PORCON <rporcon@student.42.fr>
2018-11-30 13:42:14 +0100

    get multiple workout local ok

--- services/workout/get/index.js
+++ services/workout/get/index.js
@@ -3,29 +3,28 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = (event, context, callback) => { // eslint-disable-line
-  const { id } = event.queryStringParameters;
+  const { ids } = event.queryStringParameters;
+
+  const promises = [];
   const dbParams = {
     TableName: 'workouts',
     KeyConditionExpression: '#id = :id',
     ExpressionAttributeNames: {
       '#id': '_id',
-      '#name': 'name',
     },
-    ExpressionAttributeValues: {
-      ':id': id,
-    },
-    ProjectionExpression: '#name, description',
     Limit: 1,
   };
-  dynamoDb.query(dbParams, (err, data) => {
-    if (err) {
-      callback(err);
-    } else {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({ data }),
-      };
-      callback(null, response);
-    }
+  for (let i = 0; i < ids.length; i += 1) {
+    dbParams.ExpressionAttributeValues = { ':id': ids[i] };
+    promises.push(dynamoDb.query(dbParams).promise());
+  }
+  Promise.all(promises).then((data) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    };
+    callback(null, response);
+  }).catch((err) => {
+    callback(err);
   });
 };
