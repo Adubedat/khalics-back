7247fee1f1b6e68013dbdf610a4953d6ac28b9ff
Renan PORCON <rporcon@student.42.fr>
2018-12-04 15:46:16 +0100

    /exercise/get GET ok

---
+++ services/exercise/get/index.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = (event, context, callback) => { // eslint-disable-line
+  let { ids } = event.queryStringParameters;
+
+  ids = JSON.parse(ids);
+  const promises = [];
+  const dbParams = {
+    TableName: 'exercises',
+    KeyConditionExpression: '#id = :id',
+    ExpressionAttributeNames: {
+      '#id': '_id',
+    },
+    Limit: 1,
+  };
+  for (let i = 0; i < ids.length; i += 1) {
+    dbParams.ExpressionAttributeValues = { ':id': ids[i] };
+    promises.push(dynamoDb.query(dbParams).promise());
+  }
+  Promise.all(promises).then((data) => {
+    const exercises = data.map(((val) => {
+      delete val.Items[0]._id; //eslint-disable-line
+      return val.Items[0];
+    }));
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ exercises }),
+    };
+    callback(null, response);
+  }).catch((err) => {
+    callback(err);
+  });
+};
