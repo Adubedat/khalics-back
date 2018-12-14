b6cefcdba7207fa6afc3f9ad1203fb7545d9271e
Renan PORCON <rporcon@student.42.fr>
2018-12-14 16:26:01 +0100

    add axios, jest test: create workout

--- services/tests/user.test.js
+++ services/tests/user.test.js
@@ -1,6 +1,24 @@
-test('Middle tier', () => {
-  const cost = 20000;
-  const expectedCost = 42;
+/**
+ * @jest-environment node
+ */
+const axios = require('axios');
 
-  expect(cost).toEqual(expectedCost);
+// axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://qmzsdq8495.execute-api.eu-west-1.amazonaws.com';
+
+test('Create workout', async () => {
+  const postData = {
+    description: 'TESTworkoutTESTworkoutTESTworkout',
+    name: 'TESTworkout',
+    exercises: [{
+      id: '4242', repBySet: 12, totalSet: 3, totalRound: 0,
+    },
+    {
+      id: '8484', repBySet: 10, totalSet: 3, totalRound: 0,
+    },
+    ],
+    restTime: 90,
+  };
+  const res = await axios.post('/dev/workout/create', postData);
+  expect(res.status).toEqual(200);
 });
